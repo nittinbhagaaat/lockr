e3f6b5acd8fd54d6e64a30e3ebc2d26c611009b8
Anthony <nitinbhagatxyz2@gmail.com>
2026-05-04 12:35:26 +0530

    [ADDED]: added keys for production

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://moneylockr.netlify.app/",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
